021e7c17592876275ee570ca2db15b92e33fa282
camiitomaino1 <108914799+camiitomaino1@users.noreply.github.com>
2024-11-13 21:48:29 -0300

    alert

--- Front/JS/contacto.js
+++ Front/JS/contacto.js
@@ -3,7 +3,6 @@ document.getElementById('formulario_contacto').addEventListener('submit', functi
 
     const formData = new FormData(this); 
 
-    
     fetch('http://127.0.0.1:5000/contacto', { 
         method: 'POST',
         body: formData
@@ -11,13 +10,13 @@ document.getElementById('formulario_contacto').addEventListener('submit', functi
     .then(response => response.json())  
     .then(data => {
         if (data.message) {
-            document.getElementById('responseMessage').innerText = data.message;
+            alert(data.message);
         } else {
-            document.getElementById('responseMessage').innerText = 'Hubo un error al enviar el mensaje.';
+            alert('Hubo un error al enviar el mensaje.');
         }
     })
     .catch(error => {
         console.error('Error al intentar conectar con el servidor:', error);
-        document.getElementById('responseMessage').innerText = 'Error en el servidor: ' + error.message;
+        alert('Error en el servidor: ' + error.message);
     });
 });
